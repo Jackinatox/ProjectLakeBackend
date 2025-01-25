5a691a61a3d50de19b2d35f62630056a20a54f38
Jackinatox <jakob.schulze1206@gmail.com>
2025-01-25 23:51:28 +0100

    swicthing to nextjs

--- pb_hooks/helloworld.pb.js
+++ pb_hooks/helloworld.pb.js
@@ -1,5 +1,31 @@
 /// <reference path="../data/types.d.ts" />
 
-routerAdd("GET", "/hello", (e) => {
-    return e.string(200, "Online")
+
+routerAdd("GET", "/status", (e) => {
+    return e.string(200, "Hello world!")
 })
+
+
+onRecordAfterCreateSuccess((e) => {
+    // e.app
+    // e.collection
+    // e.record
+    // and all RequestEvent fields...
+    $app.logger().warn("trying to craete pt user");
+
+    fetch(process.env.PT_PANEL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', // Indicate the format of the request body
+          },
+    }).
+
+    e.next()
+}, "users");
+
+
+onRecordCreateRequest((e) => {
+    $app.logger().warn("before" + JSON.stringify(e.record));
+
+    e.next()
+}, "users")
